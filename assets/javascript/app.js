b1569ea3868a650cec5eea10d9828b193a419825
Andrew Hok <ahok89@gmail.com>
2019-06-07 22:26:09 -0400

    added timer

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,3 +1,23 @@
+var timer = 60;
+
+var intervalId;
+
+function decrement() {
+    number--;
+    $("#timer").html("<h2>" number + "seconds left" </h2>");
+    if (number === 0) {
+        stop();
+    }
+}
+
+
+function run() {
+    intervalId = setInterval(decrement, 1000);
+}
+
+function decrement() {
+}
+
 var questions = {
     question: "Which Digidestined is revealed as the Eighth Child?",
     answers: ["Kairi", "Tai" , "T.K." , "Willis"]
@@ -39,3 +59,6 @@ var questions = {
     answers: ["Friendship", "Sincerity", "Light", "Reliability"]
     correctAnswers: "Friendship"
 }
+
+$("sub-wrapper").prepend("<h2> Time Remaining: <span id='timer'>60</span Seconds<s/h2>");
+
